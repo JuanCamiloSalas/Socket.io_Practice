f9d647479a732b7883c5f17424abbf8a5ca9eb1e
JuanCamiloSalas <juansalasprogrammer@gmail.com>
2023-03-07 07:42:02 -0500

    Emisión y escucha del servidor

--- server/server.js
+++ server/server.js
@@ -16,7 +16,33 @@ app.use(express.static(publicPath));
 let io = socketIO(server);
 
 io.on('connection', (client) => {
+
     console.log("Usuario conectado");
+
+    client.emit('enviarMensaje', {
+        usuario: "Administrador",
+        mensaje: "Bienvenido a esta app"
+    });
+
+    client.on('disconnect', () => {
+        console.log("Usuario desconectado");
+    });
+
+    // Escuchar al cliente
+    client.on('enviarMensaje', (mensaje, callback) => {
+        console.log(mensaje);
+
+        if (mensaje.usuario ) {
+            callback({
+                resp: "TODO SALIÓ BIEN"
+            });
+        } else {
+            callback({
+                resp: "TODO SALIÓ MALLL"
+            });
+        }
+
+    });
 });
 
 server.listen(port, (err) => {
